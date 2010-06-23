c519b9f14a97093725a1d5453bf6e190525249d9
Jason Heeris <jason.heeris@gmail.com>
2010-06-23 20:36:17 +0800

    Fleshed out comments. Restructured a couple of things.

--- asyncinfo2.py
+++ asyncinfo2.py
@@ -21,29 +21,80 @@ import sys
 import nautilus
 import glib
 
-# Period in seconds for really long function
+# Period in seconds for our pretend really-long-function
 TIMEOUT = 3
 
-def schedule_background_work(uri, callback):
-    glib.timeout_add_seconds(TIMEOUT, callback, uri)
-
 class ItemResult(object):
+    """
+    Associates a nautilus.FileInfo object with an eventual result. The result
+    of the asynchronous activity is stored in the "result" member.
+    """
     
     def __init__(self, nautilus_file_info):
         self.item = nautilus_file_info
         self.result = None    
 
 class AsyncInfoProvider2(nautilus.InfoProvider):
-
-    # These methods are all called synchronously. We're fine.
+    """
+    This is currently more of a template than a base class, but it is designed
+    so that the following methods can be overridden to do something useful:
+    
+    schedule_background_work() - does the asynchronous work 
+    update_info_initial() - initial response to info request
+    update_info_final() - eventual response to callback result
+    
+    Do NOT just try to inherit from it by defining a new class IN THE SAME FILE
+    and putting it where Nautilus will find it - Nautilus will see both the
+    super- and subclass as extensions and attempt to use them both.
+    """
+    
+    # Note that Nautilus calls all methods asynchronously.
 
     def __init__(self):
+        # Do not attempt to initialise super classes.
         self.nodes_awaiting_update = {}
 
+
+    def schedule_background_work(uri):
+        """
+        This gets called to do the asynchronous work. It should return as soon
+        as possible, delegating all work to another process. When it is done, it
+        should call "background_work_complete" with the URI that was given here
+        and the result from the background task.
+        
+        Do not use threads, they will not work under Nautilus.
+        
+        If scheduling the background work *still* has too much latency, I
+        recommend using glib's "idle_add" method.        
+        
+        @param uri the URI of the item for which the information was requested
+                   (this is a URL-encoded string)
+        """
+        # Let's just make something up to demonstrate
+        glib.timeout_add_seconds(
+                        TIMEOUT,
+                        self.background_work_complete,
+                        uri,
+                        42)
+
     def update_info_initial(self, item):
+        """
+        Called when the update_file_info method is called and we haven't
+        yet invoked our asynchronous activity.
+        
+        @param item a nautilus.FileInfo object
+        """
         pass
 
     def update_info_final(self, item, result):
+        """
+        Called when the update_file_info is called as a result of invalidating
+        the extension info for the given item. This means that the callback has
+        completed, the result of which is passed in.
+        
+        @param item a nautilus.FileInfo object
+        @param result the result of the asynchronous work
+        """
         if result:
             item.add_emblem("emblem-generic")
 
@@ -59,8 +110,8 @@ class AsyncInfoProvider2(nautilus.InfoProvider):
             # callback caused the status to update). Store the item, so we can
             # get it back later, and use something serialisable to map to it.
             self.nodes_awaiting_update[uri] = ItemResult(item)
-            schedule_background_work(uri, self.file_info_callback)
             self.update_info_initial(item)
+            self.schedule_background_work(uri, self.file_info_callback)
         elif item_result.result is not None:
             # This means that we are being called from INSIDE
             # invalidate_extension_info - in other words, we're in the callback
@@ -73,12 +124,24 @@ class AsyncInfoProvider2(nautilus.InfoProvider):
             # just in case it's changed, but don't do anything else.
             self.nodes_awaiting_update[uri] = ItemResult(item)
         
-    def file_info_callback(self, uri):
+    def background_work_complete(self, uri, result):
+        """
+        This handles the result of the asynchronous activity, which ends up
+        getting passed to "update_info_final()". It takes the URI that was given
+        "schedule_background_work" and a single result (that may change to a
+        more flexible user data system).
+        
+        This method is not meant to be overridden.
+        
+        @param uri the URI that was given to the scheduling method IN EXACTLY
+                   THE SAME FORM
+        @param result the result of the callback
+        """
         
         item_result = self.nodes_awaiting_update.get(uri)
         
         if item_result is not None:
-            item_result.result = True
+            item_result.result = result
             item_status.item.invalidate_extension_info()
             # Edge case: item is deleted before this callback is called
         
